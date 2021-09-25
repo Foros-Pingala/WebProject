2d0c3c402c306a5e58d0dbdc2db381f4599749fb
Pradyumna Asalkar <pradyumnaasalkar.000@gmail.com>
2021-09-25 23:24:07 +0530

    nodejs project first commit

---
+++ public/js/main.js
@@ -0,0 +1,51 @@
+const cityName = document.getElementById('cityName')
+
+const submitBtn = document.getElementById('submitBtn');
+const city_name = document.getElementById('city_name');
+const temp_real_val = document.getElementById('temp_real_val')
+const temp_status = document.getElementById('temp_status');
+
+const datahide = document.querySelector('.middle_layer');
+
+const getInfo = async(event) => {
+    event.preventDefault();
+    let cityVal = cityName.value; 
+
+    if(cityVal === ""){
+        city_name.innerText = `Please Enter City Name before Search`;
+        datahide.classList.add('data_hide');
+    }else{
+        try{
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=30a2c3981925d91b1448e2e49ca62b7b`;
+            const response = await fetch(url);
+            const data  = await response.json();
+            const arrData = [data];
+
+            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+            temp_real_val.innerText = arrData[0].main.temp;
+
+            const tempMood = arrData[0].weather[0].main;
+            
+            //condtion to check sunny or cloudy
+            if(tempMood == "Clear") {
+                temp_status.innerHTML = "<i class= 'fas fa-sun' style='color: #eccc68;'></i> "; 
+            }else if(tempMood === "Clouds"){
+                temp_status.innerHTML = "<i class= 'fas fa-cloud' style='color: #f1f2f6;'></i> ";
+            }else if(tempMood === "Rain"){
+                temp_status.innerHTML = "<i class= 'fas fa-cloud-rain 'style='color: #a4b0be;'></i> ";
+            }else{
+                temp_status.innerHTML = "<i class= 'fas fa-sun' style='color: #eccc68;'></i> "; 
+            }
+               
+            datahide.classList.remove('data_hide');
+            
+
+        }catch{
+            city_name.innerText = `Please Enter City Name properly`;
+            datahide.classList.add('data_hide');
+        }
+    }
+ 
+}
+
+submitBtn.addEventListener('click', getInfo);
